362bf172029b1121f7f55d5c55a1556455677ae7
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-17 15:56:26 -0500

    Update terraform.tf

--- labyrinth_configs/terraform config/terraform.tf
+++ labyrinth_configs/terraform config/terraform.tf
@@ -5,18 +5,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.57.0"
     }
+    /*
     hcp = {
       source  = "hashicorp/hcp"
       version = "0.17.0"
     }
+    */
   }
-
+  /*
   cloud {
     organization = "sofia_test"
     workspaces {
       name = "path-to-packer"
     }
   }
-
+  */
   required_version = "~> 1.2"
 }
