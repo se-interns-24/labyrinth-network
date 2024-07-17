8d28046a251208eb48190dfedae7e96f74a2333b
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-17 15:56:06 -0500

    Update main.tf

--- labyrinth_configs/terraform config/main.tf
+++ labyrinth_configs/terraform config/main.tf
@@ -1,70 +1,25 @@
 #defining providers
+/*
 provider "hcp" {
-}
+} */
 
 provider "aws" {
     region = var.region
 }
 
-#defining ec2 instance using module
-module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = "my-ec2-instance"
-  instance_type = var.instance_type
-  key_name = aws_key_pair.labyrinth_kp.key_name
-  ami = data.hcp_packer_image.ubuntu.cloud_image_id
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.subnet.id
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "MyEC2Instances"
-  }
-
-}
-
-#fetching information about Packer image
-data "hcp_packer_iteration" "ubuntu" {
-  bucket_name = var.hcp_bucket_ubuntu
-  channel     = var.hcp_channel
-}
-#uses above to give image info to AWS
-data "hcp_packer_image" "ubuntu" {
-  bucket_name    = data.hcp_packer_iteration.ubuntu.bucket_name
-  iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
-  cloud_provider = "aws"
-  region         = var.region
-}
-
-#generates private key to use for key pair
-resource "tls_private_key" "labyrinth" {
-  algorithm = "RSA"
-}
-#defines local var for key file name
-locals {
-  private_key_filename = "labyrinth-key.pem"
-}
-#creates key pair used to access ec2 instance
-resource "aws_key_pair" "labyrinth_kp" {
-  key_name   = local.private_key_filename
-  public_key = tls_private_key.labyrinth.public_key_openssh
-}
-
-#defines a virtual private cloud - isolated network to launch ec2 instance
+#defines a virtual private cloud - isolated network to launch ec2 instance - VPC/networking
 resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
-#creates gateway that allows VPC to talk to internet
+#creates gateway that allows VPC to talk to internet - VPC/networking & down
 resource "aws_internet_gateway" "gw"{
   vpc_id = aws_vpc.vpc.id
 }
 
-#defines subnet within vpc to place resources
+#defines subnet within vpc to place resources 
 resource "aws_subnet" "subnet"{
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.cidr_subnet
@@ -114,9 +69,3 @@ resource "aws_security_group" "sg"{
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-#outputs app url
-output "app_url" {
-  value = "http://${module.ec2_instance.public_ip}"
-}
-
