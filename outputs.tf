c76484e0b3f5003d4c257518b4b60bae1c16f247
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-23 15:31:41 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,9 +8,19 @@ output "security_group_id" {
   value       = [aws_security_group.sg.id]
 }
 
+output "rds_security_group_id" {
+  description = "Security groups for RDS"
+  value       = [aws_security_group.rds-sg.id]
+}
+
 output "subnet_id" {
   description = "Subnet IDs"
   value       = aws_subnet.subnet.*.id
+}
+
+output "subnet_group_name" {
+  description = "Name of the DB subnet group"
+  value       = aws_db_subnet_group.subnet-group.name
 }
 /*
 output "public_dns_name" {
