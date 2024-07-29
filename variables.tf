be940e56aa49bcd6cbf8e15b8cc9fd43d6eb0e78
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-29 16:17:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,8 +17,8 @@ variable "cidr_subnet"{
   description = "CIDR block for the subnet"
   default = "10.1.0.0/24" //allows to divide VPC into smaller & manageable segments
 }
-/*
+
 variable "cidr_subnet_b"{
   description = "CIDR block for the subnet"
   default = "10.1.2.0/24" //allows to divide VPC into smaller & manageable segments
-}*/
+}
