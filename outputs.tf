49c02acfa6ddf502c4760d2fc80a6e573b4667cf
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-31 10:39:53 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "subnet_id" {
   value       = aws_subnet.subnet.*.id
 }
 
+output "subnet_id_b" {
+  description = "Subnet IDs"
+  value       = aws_subnet.subnet_b.*.id
+}
+
 output "subnet_group_name" {
   description = "Name of the DB subnet group"
   value       = aws_db_subnet_group.subnet-group.name
