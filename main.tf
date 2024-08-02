6876376fc69439c80152c1ea55a7b2fd5647e494
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-02 16:02:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
 #creates gateway that allows VPC to talk to internet - VPC/networking & down
