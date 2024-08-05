762c0daff102c080d6dada9ca75a3cda0e37c3d6
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-05 13:04:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-east-2"
 }
 
 variable "vpc_name" {
@@ -10,12 +9,10 @@ variable "vpc_name" {
 
 variable "availability_zone" {
   description = "Subnet A AZ"
-  default     = "us-east-2c"
 }
 
 variable "availability_zone_b" {
   description = "Subnet B AZ"
-  default     = "us-east-2b"
 }
 
 variable "subnet_group_name" {
