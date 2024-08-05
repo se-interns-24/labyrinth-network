dc946d2b0c4c6607182e21683b9190c53b7f52c9
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-05 10:53:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,26 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
+variable "vpc_name" {
+  description = "VPC name dev"
+  default     = "labyrinth-vpc"
+}
+
+variable "availability_zone" {
+  description = "Subnet A AZ"
+  default     = "us-west-1c"
+}
+
+variable "availability_zone_b" {
+  description = "Subnet B AZ"
+  default     = "us-west-1b"
+}
+
+variable "subnet_group_name" {
+  description = "DB subnet group name"
+  default     = "db-subnet-group"
 }
 
 variable "cidr_vpc" {
@@ -16,4 +31,24 @@ variable "cidr_vpc" {
 variable "cidr_subnet"{
   description = "CIDR block for the subnet"
   default = "10.1.0.0/24" //allows to divide VPC into smaller & manageable segments
+}
+
+variable "cidr_subnet_b"{
+  description = "CIDR block for the subnet"
+  default = "10.1.2.0/24" //allows to divide VPC into smaller & manageable segments
+}
+
+variable "cidr_route_table" {
+  description = "CIDR block for the route table"
+  default     = "0.0.0.0/0"
+}
+
+variable "security_group_name" {
+  description = "Name for app's security group"
+  default     = "sg"
+}
+
+variable "rds_security_group_name" {
+  description = "Name for DB's security group"
+  default     = "rds_sg"
 }
